34c9161c363b49f66569453b2bbb276c47c27a63
Simon Smith <simonsmith5521@gmail.com>
2022-03-22 17:03:56 +0000

    Update netclient-install.ps1

--- scripts/netclient-install.ps1
+++ scripts/netclient-install.ps1
@@ -41,22 +41,17 @@ new-module -name netclient-install -scriptblock {
                 Write-Host "https://github.com/gravitl/netmaker/releases/download/$version/netclient.exe";
                 $url = "https://github.com/gravitl/netmaker/releases/download/$version/netclient.exe"
                 Invoke-WebRequest -Uri $url -OutFile $outpath
+                New-Item -Path "C:\ProgramData\" -Name "Netclient" -ItemType "directory"
+                Move-Item -Path "$env:userprofile\Downloads\netclient.exe" -Destination "C:\ProgramData\Netclient\netclient.exe"
+                $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+                newpath = "$oldpath;C:\ProgramData\Netclient"
+                Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+                $env:Path += ";C:\ProgramData\Netclient"
+                Write-Host "'netclient' is installed."
             }
-            $runNum = "one"
-            foreach ($run in $runNum) { 
-                if ((Get-Command "netclient.exe" -ErrorAction SilentlyContinue) -eq $null) { 
-                    if (-not (Test-Path -Path "C:\ProgramData\Netclient\netclient.exe")) {
-                        Move-Item -Path "$env:userprofile\Downloads\netclient.exe" -Destination "C:\ProgramData\Netclient\netclient.exe"
-                        $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-                        $newpath = "$oldpath;C:\ProgramData\Netclient"
-                        Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
-                        $env:Path += ";C:\ProgramData\Netclient"
-                    }
-                }
-                $NetArgs = @("join","-t",$token)
-                Start-Process -Filepath $outpath -ArgumentList $NetArgs -Wait
-            }
-        Start-Sleep -s 5
-        Write-Host "'netclient' is installed."
+            $NetArgs = @("join","-t",$token)
+            Start-Process -Filepath $outpath -ArgumentList $NetArgs -Wait
+            Start-Sleep -s 5
+            Write-Host "'netclient' has been setup."
     }
 }
