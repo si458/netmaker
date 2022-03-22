87cdea158fe8a9e386359a92438104b38f668f43
Simon Smith <simonsmith5521@gmail.com>
2022-03-22 16:37:18 +0000

    Update netclient-install.ps1

--- scripts/netclient-install.ps1
+++ scripts/netclient-install.ps1
@@ -37,8 +37,8 @@ new-module -name netclient-install -scriptblock {
                 Write-Host "'$software' is installed."
             }
             $outpath = "";
-            if (Test-Path -Path "C:\ProgramData\Netclient\bin\netclient.exe") {
-                $outpath = "C:\ProgramData\Netclient\bin\netclient.exe";
+            if (Test-Path -Path "C:\ProgramData\Netclient\netclient.exe") {
+                $outpath = "C:\ProgramData\Netclient\netclient.exe";
             } else {
                 $outpath = "$env:userprofile\Downloads\netclient.exe"
                 Write-Host "'netclient.exe' is NOT installed. installing...";
@@ -53,13 +53,12 @@ new-module -name netclient-install -scriptblock {
                 Start-Process -Filepath $outpath -ArgumentList $NetArgs -Wait
 
                 if ((Get-Command "netclient.exe" -ErrorAction SilentlyContinue) -eq $null) { 
-                    if (-not (Test-Path -Path "C:\ProgramData\Netclient\bin\netclient.exe")) {
-                        New-Item -Path "C:\ProgramData\Netclient" -Name "bin" -ItemType "directory"
-                        Move-Item -Path "$env:userprofile\Downloads\netclient.exe" -Destination "C:\ProgramData\Netclient\bin\netclient.exe"
+                    if (-not (Test-Path -Path "C:\ProgramData\Netclient\netclient.exe")) {
+                        Move-Item -Path "$env:userprofile\Downloads\netclient.exe" -Destination "C:\ProgramData\Netclient\netclient.exe"
                         $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-                        $newpath = "$oldpath;C:\ProgramData\Netclient\bin"
+                        $newpath = "$oldpath;C:\ProgramData\Netclient"
                         Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
-                        $env:Path += ";C:\ProgramData\Netclient\bin"
+                        $env:Path += ";C:\ProgramData\Netclient"
                     }
                 }
                 #if($run -eq "one"){
